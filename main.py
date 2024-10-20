96906c508878d2b63d2f2d97d060019f27274cf7
Vinay Ram Gazula <vinayramgazula@gmail.com>
2024-10-19 22:17:44 -0400

    basic structure;

---
+++ main.py
@@ -0,0 +1,6 @@
+# main.py
+from calculator import Calculator
+
+# You must put this in your main.py because this forces the program to start when you run it from the command line.
+if __name__ == "__main__":
+    app = Calculator().run()  # Instantiate an instance of App
