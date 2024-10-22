92d1754bff1e08bf036d2887f8f19a5a5d6f9f4b
Vinay Ram Gazula <vinayramgazula@gmail.com>
2024-10-22 06:31:31 -0400

    limited the console output to CRITICAL level; pytest passes with 98% coverage;

--- calculator/logger.py
+++ calculator/logger.py
@@ -1,11 +1,12 @@
 import os
 import logging, logging.config
 
-def configure_logging(log_dir: str='logs',logging_conf_path: str='logging.conf'):
-    os.makedirs(log_dir, exist_ok=True)
+def configure_logging(log_path: str = 'logs', logging_conf_path: str = 'logging.conf'):
+    os.makedirs(log_path, exist_ok=True)
     logging.config.fileConfig(logging_conf_path) if os.path.exists(logging_conf_path) else logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     logging.info(f"Logging configured using {'fileConfig' if os.path.exists(logging_conf_path) else 'basicConfig'} method.")
 
-# we can import these loggers in any module to log using these specific loggers
-#info_logger = logging.getLogger('info')
+#logging.getLogger('root').addFilter(logging.Filter('info'))
+#info_logger = logging.getLogger('info').addFilter(logging.Filter('error'))
 #error_logger = logging.getLogger('error')
+
